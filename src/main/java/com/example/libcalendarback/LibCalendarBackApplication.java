02ffb25b52dbaa43b41f3565644e8f19b1cc9aba
Célian Frasca <celian.frasca@gmail.com>
2023-10-10 16:46:56 +0200

    first commit

---
+++ src/main/java/com/example/libcalendarback/LibCalendarBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.libcalendarback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibCalendarBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibCalendarBackApplication.class, args);
+	}
+
+}
