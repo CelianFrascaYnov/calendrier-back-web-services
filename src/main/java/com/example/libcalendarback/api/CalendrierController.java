791950c1f0572002430ec7ec398cebb51d4e93f7
Célian Frasca <celian.frasca@gmail.com>
2023-10-10 16:47:51 +0200

    Aout API

--- src/main/java/com/example/libcalendarback/api/CalendrierController.java
+++ src/main/java/com/example/libcalendarback/api/CalendrierController.java
@@ -1,2 +1,23 @@
-package com.example.libcalendarback.Api;public class CalendrierController {
+package com.example.libcalendarback.api;
+
+import com.example.libcalendar.CalendrierGenerator;
+import com.example.libcalendar.model.MoisCalendrier;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class CalendrierController {
+
+    @GetMapping("/calendrier")
+    public MoisCalendrier getCalendrier(@RequestParam int annee, @RequestParam int mois) {
+        // Utilise la fonction generateCalendrier de votre bibliothèque
+        CalendrierGenerator calendrierGenerator = new CalendrierGenerator();
+        MoisCalendrier calendrier = calendrierGenerator.generateCalendrier(annee, mois);
+
+        // Retourne le calendrier en tant que réponse
+        return calendrier;
+    }
 }
